47e4c01b970b37911829e1cd9f3473fb06297047
Maxel <gigaabait@yandex.ru>
2020-06-06 17:33:07 +0400

    almost done

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+// сделать треугольник, трапецию, ромб
 const canvas = document.getElementById('myCanvas'),
       ctx = canvas.getContext("2d");
 
 
 class Line {
-    constructor(coords, color = "#000", lineWidth = 10){
+    constructor(coords, color, lineWidth = 10){
         this.startX = coords.startX;
         this.startY = coords.startY;
         this.endX = coords.endX;
@@ -22,7 +23,7 @@ class Line {
     }
 }
 class Rectangle {
-    constructor(coords, borderWidth = 0, color = "#000", background = "rgba(255, 0, 0, 0.6)"){
+    constructor(coords, borderWidth = 0, color, background){
         this.startX = coords.startX;
         this.startY = coords.startY;
         this.endX = coords.endX;
@@ -33,8 +34,6 @@ class Rectangle {
     }
     draw(){
             //draw figure
-            // ctx.fillStyle=this.background;
-            // ctx.fillRect(this.startX, this.startY, this.width, this.height);
             ctx.beginPath();
             ctx.moveTo(this.startX, this.startY);
             ctx.lineTo(this.endX, this.startY);
@@ -59,7 +58,7 @@ class Rectangle {
 }
 
 class Triangle {
-    constructor(coords, borderWidth = 0, color = "#000", background = "rgba(255, 0, 0, 1)") {
+    constructor(coords, borderWidth = 0, color, background) {
         this.firstX = coords.firstX;
         this.firstY = coords.firstY;
         this.secondX = coords.secondX;
@@ -94,7 +93,7 @@ class Triangle {
     }
 }
 class Circle {
-    constructor(coords, borderWidth = 0, color = "#000", background = "rgba(255, 0, 0, 1)") {
+    constructor(coords, borderWidth = 0, color, background) {
         this.startX = coords.startX;
         this.startY = coords.startY;
         this.radius = coords.radius;
@@ -122,7 +121,7 @@ class Circle {
     }
 }
 class Ellipse {
-    constructor(coords, borderWidth = 0, color = "#000", background = "rgba(255, 0, 0, 1)") {
+    constructor(coords, borderWidth = 0, color, background) {
         this.startX = coords.startX;
         this.startY = coords.startY;
         this.radiusX = coords.radiusX;
@@ -148,29 +147,43 @@ class Ellipse {
         }
     }
 }
-// line -p [50, 50] [100, 100] -c rgb(255, 0, 0) -b rgba(0, 255, 0, 0.3)
+// line -p [50, 50] [100, 100] -c hsl(0, 100%, 50%)
+// circle -p [75, 75] -r1 25 -c rgb(255, 0, 0) -b rgba(0, 255, 0, 0.3)
+// ellipse -p [75, 75] -rot 6 -r1 50 -r2 25 -c rgb(0, 255, 0) -b rgba(255, 0, 0, 0.3)
 const input = document.getElementById('command-input'),
       btn = document.getElementById('input-btn');
-
+      clear = document.getElementById('clear-btn');
+clear.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+})
 btn.addEventListener('click', () => {
     let string = input.value;
     
             let newString = string.replace(/\s+/g, ''),
-            indexP = newString.indexOf('-p'),
-            indexC = newString.indexOf('-c'),
-            indexB = newString.indexOf('-b'),
-            coordsString = '',
-            colorString = 'rgb(255, 0, 0)',
-            backgroundString = 'rgba(255, 0, 0, 1)';
-            console.log(newString, indexP, indexC)
+                indexP = newString.indexOf('-p'),
+                indexC = newString.indexOf('-c'),
+                indexB = newString.indexOf('-b'),
+                indexR1 = newString.indexOf('-r1'),
+                indexR2 = newString.indexOf('-r2'),
+                indexRot = newString.indexOf('-rot'),
+                indexW = newString.indexOf('-w'),
+                coordsString = '',
+                colorString = 'black',
+                r1String = '50',
+                r2String = '25',
+                rotString = '2',
+                widthString = '10',
+                backgroundString = 'white';
+            console.log(newString, indexP, indexC, indexR1, indexR2, indexRot, indexW)
+            
             if(indexC != -1) {
                 coordsString = newString.slice(indexP + 2, indexC);
-            if(indexB != -1) {
-                colorString = newString.slice(indexC + 2, indexB);
-                backgroundString = newString.slice(indexB + 2);
-            } else {
-                colorString = newString.slice(indexC + 2);
-            }
+                if(indexB != -1) {
+                    colorString = newString.slice(indexC + 2, indexB);
+                    backgroundString = newString.slice(indexB + 2);
+                } else {
+                    colorString = newString.slice(indexC + 2);
+                }
             } else if(indexB != -1) {
                 coordsString = newString.slice(indexP + 2, indexB);
                 backgroundString = newString.slice(indexB + 2);
@@ -178,13 +191,110 @@ btn.addEventListener('click', () => {
             else {
                 coordsString = newString.slice(indexP + 2);
             }
-        // colorString = newString.slice(indexC + 2);
-        console.log(coordsString.split(']['));
+            if(indexR1 != -1) {
+                if(indexR2 != -1) {
+                    r1String = newString.slice(indexR1 + 3, indexR2)
+                }else if(indexC != -1) {
+                    r1String = newString.slice(indexR1 + 3, indexC)
+                } else if(indexB != -1) {
+                    r1String = newString.slice(indexR1 + 3, indexB)
+                } else {
+                    r1String = newString.slice(indexR1 + 3)
+                }   
+            }
+            if(indexR2 != -1) {
+                r2String = newString.slice(indexR2 + 3, indexC)
+            } else if(indexC != -1) {
+                r2String = newString.slice(indexR2 + 3, indexB)
+            }  else {
+                r1String = newString.slice(indexR1 + 3)
+            }
+            if(indexRot != -1) {
+                rotString =  newString.slice(indexRot + 4, indexR1);
+            }
+            if(indexW != -1) {
+                if(indexRot != -1) {
+                    widthString = newString.slice(indexW + 2, indexRot)
+                } else if (indexR1 != -1) {
+                    widthString = newString.slice(indexW + 2, indexR1)
+                } else if (indexR2 != -1) {
+                    widthString = newString.slice(indexW + 2, indexR2)
+                } else if (indexC != -1) {
+                    widthString = newString.slice(indexW + 2, indexC)
+                } else if (indexB != -1) {
+                    widthString = newString.slice(indexW + 2, indexB)
+                } else {
+                    widthString = newString.slice(indexW + 2)
+                }
+                
+            }
+
+        coordsString = coordsString.replace(/\[+/g, '').replace(/\]+/g, ',');
+        coords = coordsString.split(',')
+        coords.pop();
+
+        if(string.includes('line')) {
+            let line = new Line(
+                coords = {
+                    startX: Number(coords[0]),
+                    startY: Number(coords[1]),
+                    endX: Number(coords[2]),
+                    endY: Number(coords[3])
+                    },
+                    color = colorString,
+                    lineWidth = Number(widthString)
+            )
+            line.draw();
+        }
+        if(string.includes('rectangle')) {
+            let rectangle = new Rectangle(
+                    coords = {
+                        startX: Number(coords[0]),
+                        startY: Number(coords[1]),
+                        endX: Number(coords[2]),
+                        endY: Number(coords[3])
+                    },
+                        borderWidth = Number(widthString),
+                        color = colorString,
+                        background = backgroundString,
+            )
+            rectangle.draw();
+        }
+        if(string.includes('circle')) {
+            let circle = new Circle(
+                    coords = {
+                             startX: Number(coords[0]),
+                             startY: Number(coords[1]),
+                             radius: Number(r1String)
+                    },
+                        borderWidth = Number(widthString),
+                        color = colorString,
+                        background = backgroundString,
+            )
+            circle.draw();
+        }
+        if(string.includes('ellipse')) {
+            let ellipse = new Ellipse(
+                    coords = {
+                             startX: Number(coords[0]),
+                             startY: Number(coords[1]),
+                             radiusX: Number(r1String),
+                             radiusY: Number(r2String),
+                             rotation: Math.PI / Number(rotString)
+                    },
+                        borderWidth = Number(widthString),
+                        color = colorString,
+                        background = backgroundString,
+            )
+            ellipse.draw();
+        }
+        console.log(coords);
+        console.log(coordsString);
+        
         console.log(colorString);
         console.log(backgroundString);
-    
-     
-    // .split('–c').split('–b')
+        console.log(r1String);
+        console.log(rotString);
 
 })
 // const ellipse = new Ellipse (
